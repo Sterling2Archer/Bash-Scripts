5f0c104cd4765e3de636cba5685daf69c7f5a938
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2022-07-30 00:09:48 -0400

    Add files via upload

---
+++ KillThisLove.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Lucky me, lucky you"
+
+sleep 1
+
+echo "결국엔 거짓말, we lie"
+
+sleep 2
+
+echo "so what"
+
+echo "so what"
+
+sleep 5
+
+echo "let's kill this love!"
+
+sleep 8
+
+echo "rum pa pum ra pa pummmm"
+
+echo "let's kill this love!"
+
+sleep 8
+
+echo "rum pa pum ra pa pummmm"
+
+exit
+
+
+
+
---
+++ stilldre.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "still taking my time to perfect the beat"
+
+echo "and I still got love for the streets"
+
+sleep 3
+
+echo "it's the D.R.E."
+
+exit
+
